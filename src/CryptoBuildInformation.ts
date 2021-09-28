2a011cfb6022094c8380e62342438fb4d0363cc8
Timo Notheisen <timo.notheisen@js-soft.com>
2021-09-28 09:58:01 +0200

    refactor: export json instead of class

--- src/CryptoBuildInformation.ts
+++ src/CryptoBuildInformation.ts
@@ -1,21 +1,17 @@
 import { ServalBuildInformation } from "@js-soft/ts-serval";
 
-export class CryptoBuildInformation {
-    public readonly version: string = "{{version}}";
-    public readonly build: string = "{{build}}";
-    public readonly date: string = "{{date}}";
-    public readonly commit: string = "{{commit}}";
-    public readonly dependencies: object;
-    public readonly serval: ServalBuildInformation = ServalBuildInformation.info;
+const buildInformation = {
+    version: "{{version}}",
+    date: "{{date}}",
+    commit: "{{commit}}",
+    dependencies: {},
+    serval: ServalBuildInformation.info
+};
 
-    private constructor() {
-        try {
-            // eslint-disable-next-line @typescript-eslint/quotes
-            this.dependencies = JSON.parse(`{{dependencies}}`);
-        } catch (e) {
-            this.dependencies = {};
-        }
-    }
-
-    public static readonly info: CryptoBuildInformation = new CryptoBuildInformation();
+try {
+    buildInformation.dependencies = JSON.parse(`{{dependencies}}`); // eslint-disable-line @typescript-eslint/quotes
+} catch (e) {
+    // Leave the default value
 }
+
+export default buildInformation;
