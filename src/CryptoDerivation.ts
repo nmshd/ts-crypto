dc67faec513fc21fe6b8a25714e820f977dce56b
stnmtz <t.stnmtz@outlook.de>
2024-10-21 18:03:20 +0200

    feat: deriveKeyFromPassword

--- src/CryptoDerivation.ts
+++ src/CryptoDerivation.ts
@@ -1,4 +1,4 @@
-import { CoreBuffer, Encoding, ICoreBuffer } from "./CoreBuffer";
+import { CoreBuffer, ICoreBuffer } from "./CoreBuffer";
 import { CryptoEncryptionAlgorithm } from "./encryption/CryptoEncryption";
 import { CryptoSecretKey } from "./encryption/CryptoSecretKey";
 import { SodiumWrapper } from "./SodiumWrapper";
@@ -7,7 +7,8 @@ import { SodiumWrapper } from "./SodiumWrapper";
  * The key derivation algorithm to use
  */
 export const enum CryptoDerivationAlgorithm {
-    PBKDF2 = "pbkdf2"
+    ARGON2I = "argon2i",
+    ARGON2ID = "argon2id"
 }
 
 export interface ICryptoDerivation {}
@@ -15,11 +16,13 @@ export interface ICryptoDerivation {}
 export interface ICryptoDerivationStatic {
     new (): ICryptoDerivation;
 
-    deriveKeyFromMaster(
+    deriveKeyFromPassword(
         masterKey: ICoreBuffer,
-        iterations: number,
+        salt: ICoreBuffer,
         keyAlgorithm: CryptoEncryptionAlgorithm,
-        salt?: ICoreBuffer
+        derivationAlgorithm:CryptoDerivationAlgorithm,
+        iterations: number,
+        memlimit: number
     ): Promise<CryptoSecretKey>;
     deriveKeyFromBase(
         baseKey: ICoreBuffer,
@@ -30,30 +33,60 @@ export interface ICryptoDerivationStatic {
 }
 
 export class CryptoDerivation implements ICryptoDerivation {
-    public static async deriveKeyFromMaster(
-        masterKey: ICoreBuffer,
-        iterations: number,
-        keyAlgorithm: CryptoEncryptionAlgorithm,
-        salt?: ICoreBuffer
+    public static async deriveKeyFromPassword(
+        password: ICoreBuffer,
+        salt: ICoreBuffer,
+        keyAlgorithm: CryptoEncryptionAlgorithm = CryptoEncryptionAlgorithm.XCHACHA20_POLY1305,
+        derivationAlgorithm:CryptoDerivationAlgorithm = CryptoDerivationAlgorithm.ARGON2ID,
+        opslimit: number = 100000,
+        memlimit: number = 8192
     ): Promise<CryptoSecretKey> {
+        const sodium:any = await SodiumWrapper.ready() as any
+        if (salt.buffer.byteLength !== 16) {
+            throw new Error(`The salt must be exactly ${sodium.crypto_pwhash_SALTBYTES} bytes long!`);
+        }
+        if (opslimit < sodium.crypto_pwhash_OPSLIMIT_MIN) {
+            throw new Error(`The opslimit must be higher than ${sodium.crypto_pwhash_OPSLIMIT_MIN}.`)
+        }
+        if (sodium.crypto_pwhash_OPSLIMIT_MAX > 0 && opslimit > sodium.crypto_pwhash_OPSLIMIT_MAX) {
+            throw new Error(`The opslimit must be lower than ${sodium.crypto_pwhash_OPSLIMIT_MAX}.`)
+        }
+        if (memlimit < sodium.crypto_pwhash_MEMLIMIT_MIN) {
+            throw new Error(`The memlimit must be higher than ${sodium.crypto_pwhash_MEMLIMIT_MIN}.`)
+        }
+        if (sodium.crypto_pwhash_MEMLIMIT_MAX > 0 && memlimit > sodium.crypto_pwhash_MEMLIMIT_MAX) {
+            throw new Error(`The memlimit must be lower than ${sodium.crypto_pwhash_MEMLIMIT_MAX}.`)
+        }
+
+        
+
+        let keyLength;
         switch (keyAlgorithm) {
             case CryptoEncryptionAlgorithm.AES128_GCM:
+                keyLength = 16;
+                break;
             case CryptoEncryptionAlgorithm.AES256_GCM:
             case CryptoEncryptionAlgorithm.XCHACHA20_POLY1305:
+                keyLength = 32;
                 break;
             default:
                 throw new Error("KeyAlgorithm not supported.");
         }
-        const saltedMaster: ICoreBuffer = masterKey;
 
-        if (typeof salt !== "undefined") {
-            saltedMaster.append(salt);
+        let derivationAlgorithmAsNumber:number;
+        switch (derivationAlgorithm) {
+            case CryptoDerivationAlgorithm.ARGON2I:
+                derivationAlgorithmAsNumber = 1;
+                break;
+            case CryptoDerivationAlgorithm.ARGON2ID:
+                derivationAlgorithmAsNumber = 2;
+                break;
+            default:
+                throw new Error("DerivationAlgorithm not supported.");
         }
 
-        const memLimit = 2000;
-
-        const pwhash = (await SodiumWrapper.ready()).crypto_pwhash_str(saltedMaster.buffer, iterations, memLimit);
-        const hashBuffer = CoreBuffer.fromString(pwhash, Encoding.Hex);
+        const pwhash = (await SodiumWrapper.ready()).crypto_pwhash(keyLength, password.buffer, salt.buffer, opslimit, memlimit, derivationAlgorithmAsNumber);
+        const hashBuffer = CoreBuffer.from(pwhash);
         return CryptoSecretKey.from({ secretKey: hashBuffer, algorithm: keyAlgorithm });
     }
 
---
+++ test/crypto/CryptoDerivationTest.test.ts
@@ -0,0 +1,143 @@
+import { CoreBuffer, CryptoDerivation, CryptoDerivationAlgorithm, CryptoEncryptionAlgorithm, CryptoHash, CryptoHashAlgorithm, CryptoSecretKey, Encoding, ICoreBuffer } from "@nmshd/crypto";
+import { expect } from "chai";
+
+export class CryptoDerivationTest {
+    public static run(): void {
+        describe("CryptoDerivation", function () {
+
+            describe("Execute deriveKeyFromBase()", function () {
+                let keybuffer: ICoreBuffer;
+                before(async function () {
+                    // Create 256bit entropy
+                    keybuffer = await CryptoHash.hash(CoreBuffer.fromString("test", Encoding.Utf8), CryptoHashAlgorithm.SHA256);
+                });
+
+                it("should derive the same key of same input", async function() {
+                    const derived = await CryptoDerivation.deriveKeyFromBase(keybuffer, 0, "12345678")
+                    expect(derived).to.be.instanceOf(CryptoSecretKey);
+                    expect(derived.toBase64()).to.equal("eyJrZXkiOiJfdkg4NkdJeU12cFdnZkJ2Y09FY0dfNHRhakpiVXJ5cGRUWW9iZl9jb0Y4IiwiYWxnIjozLCJAdHlwZSI6IkNyeXB0b1NlY3JldEtleSJ9")
+
+                    const derivedComparison = await CryptoDerivation.deriveKeyFromBase(keybuffer, 0, "12345678")
+                    expect(derivedComparison).to.be.instanceOf(CryptoSecretKey);
+                    expect(derivedComparison.toBase64()).to.equal("eyJrZXkiOiJfdkg4NkdJeU12cFdnZkJ2Y09FY0dfNHRhakpiVXJ5cGRUWW9iZl9jb0Y4IiwiYWxnIjozLCJAdHlwZSI6IkNyeXB0b1NlY3JldEtleSJ9")
+
+                    expect(derived.toBase64()).to.equal(derivedComparison.toBase64())
+                })
+
+                it("should derive different keys of different input (basekey)", async function() {
+                    const derived = await CryptoDerivation.deriveKeyFromBase(keybuffer, 0, "12345678")
+                    expect(derived).to.be.instanceOf(CryptoSecretKey);
+                    expect(derived.toBase64()).to.equal("eyJrZXkiOiJfdkg4NkdJeU12cFdnZkJ2Y09FY0dfNHRhakpiVXJ5cGRUWW9iZl9jb0Y4IiwiYWxnIjozLCJAdHlwZSI6IkNyeXB0b1NlY3JldEtleSJ9")
+
+                    const keybuffer2 = await CryptoHash.hash(CoreBuffer.fromString("test2", Encoding.Utf8), CryptoHashAlgorithm.SHA256); 
+                    const derivedComparison = await CryptoDerivation.deriveKeyFromBase(keybuffer2, 0, "12345678")
+                    expect(derivedComparison).to.be.instanceOf(CryptoSecretKey);
+                    expect(derivedComparison.toBase64()).to.equal("eyJrZXkiOiJna0RvRTFFWjNnRHBxb3VxalNRcjJ3UzB1Wms5dzRSTkdlNkZDZkRodnBZIiwiYWxnIjozLCJAdHlwZSI6IkNyeXB0b1NlY3JldEtleSJ9")
+
+                    expect(derived.toBase64()).to.not.equal(derivedComparison.toBase64())
+                })
+
+                it("should derive different keys of different input (context)", async function() {
+                    const derived = await CryptoDerivation.deriveKeyFromBase(keybuffer, 0, "12345678")
+                    expect(derived).to.be.instanceOf(CryptoSecretKey);
+                    expect(derived.toBase64()).to.equal("eyJrZXkiOiJfdkg4NkdJeU12cFdnZkJ2Y09FY0dfNHRhakpiVXJ5cGRUWW9iZl9jb0Y4IiwiYWxnIjozLCJAdHlwZSI6IkNyeXB0b1NlY3JldEtleSJ9")
+
+                    const derivedComparison = await CryptoDerivation.deriveKeyFromBase(keybuffer, 0, "x2345678")
+                    expect(derivedComparison).to.be.instanceOf(CryptoSecretKey);
+                    expect(derivedComparison.toBase64()).to.equal("eyJrZXkiOiJjY3Nsenk2eml5NnVJdVRocHVmaWZvV1cybzBIYmFPNlFFUXR1ZTRJOS1ZIiwiYWxnIjozLCJAdHlwZSI6IkNyeXB0b1NlY3JldEtleSJ9")
+
+                    expect(derived.toBase64()).to.not.equal(derivedComparison.toBase64())
+                })
+
+                it("should derive different keys of different input (keyId)", async function() {
+                    const derived = await CryptoDerivation.deriveKeyFromBase(keybuffer, 0, "12345678")
+                    expect(derived).to.be.instanceOf(CryptoSecretKey);
+                    expect(derived.toBase64()).to.equal("eyJrZXkiOiJfdkg4NkdJeU12cFdnZkJ2Y09FY0dfNHRhakpiVXJ5cGRUWW9iZl9jb0Y4IiwiYWxnIjozLCJAdHlwZSI6IkNyeXB0b1NlY3JldEtleSJ9")
+
+                    const derivedComparison = await CryptoDerivation.deriveKeyFromBase(keybuffer, 1, "12345678")
+                    expect(derivedComparison).to.be.instanceOf(CryptoSecretKey);
+                    expect(derivedComparison.toBase64()).to.equal("eyJrZXkiOiJlMlNpUGlkMEtITVV2RzZzZ2VIX2FDM1hHTWJtd3ZuYnAzbXVvY3JMSEVBIiwiYWxnIjozLCJAdHlwZSI6IkNyeXB0b1NlY3JldEtleSJ9")
+
+                    expect(derived.toBase64()).to.not.equal(derivedComparison.toBase64())
+                })
+            });
+
+
+            describe("Execute deriveKeyFromPassword()", function () {
+                let master: ICoreBuffer;
+                let salt: ICoreBuffer;
+                before(async function () {
+                    // Create 256bit entropy
+                    master = CoreBuffer.fromUtf8("test")
+                    salt = CoreBuffer.from((await CryptoHash.hash(CoreBuffer.fromString("test", Encoding.Utf8), CryptoHashAlgorithm.SHA256)).buffer.subarray(0,16));
+                });
+
+                it("should derive the same key of same input", async function() {
+                    
+                    const derived = await CryptoDerivation.deriveKeyFromPassword(master, salt, CryptoEncryptionAlgorithm.XCHACHA20_POLY1305, CryptoDerivationAlgorithm.ARGON2ID, 10)
+                    expect(derived).to.be.instanceOf(CryptoSecretKey);
+                    expect(derived.toBase64()).to.equal("eyJrZXkiOiI5ck1uY2NOODlsRVpXNVJuQWdpWWk4Tm9xY21vOWIyMmFYQmpuMTlRV0ZRIiwiYWxnIjozLCJAdHlwZSI6IkNyeXB0b1NlY3JldEtleSJ9")
+
+                    const derivedComparison = await await CryptoDerivation.deriveKeyFromPassword(master, salt, CryptoEncryptionAlgorithm.XCHACHA20_POLY1305, CryptoDerivationAlgorithm.ARGON2ID, 10)
+                    expect(derivedComparison).to.be.instanceOf(CryptoSecretKey);
+                    expect(derivedComparison.toBase64()).to.equal("eyJrZXkiOiI5ck1uY2NOODlsRVpXNVJuQWdpWWk4Tm9xY21vOWIyMmFYQmpuMTlRV0ZRIiwiYWxnIjozLCJAdHlwZSI6IkNyeXB0b1NlY3JldEtleSJ9")
+
+                    expect(derived.toBase64()).to.equal(derivedComparison.toBase64())
+                })
+
+                it("should derive different keys of different input (master)", async function() {
+                    const derived = await await CryptoDerivation.deriveKeyFromPassword(master, salt, CryptoEncryptionAlgorithm.XCHACHA20_POLY1305, CryptoDerivationAlgorithm.ARGON2ID, 10)
+                    expect(derived).to.be.instanceOf(CryptoSecretKey);
+                    expect(derived.toBase64()).to.equal("eyJrZXkiOiI5ck1uY2NOODlsRVpXNVJuQWdpWWk4Tm9xY21vOWIyMmFYQmpuMTlRV0ZRIiwiYWxnIjozLCJAdHlwZSI6IkNyeXB0b1NlY3JldEtleSJ9")
+
+                    const master2 = CoreBuffer.fromString("test2", Encoding.Utf8)
+                    const derivedComparison = await CryptoDerivation.deriveKeyFromPassword(master2, salt, CryptoEncryptionAlgorithm.XCHACHA20_POLY1305, CryptoDerivationAlgorithm.ARGON2ID, 10)
+                    expect(derivedComparison).to.be.instanceOf(CryptoSecretKey);
+                    expect(derivedComparison.toBase64()).to.equal("eyJrZXkiOiJZaUNROFlTUkd2YmNUMExZeWU0NkxqZ0EySkpEaEVIQjRoNEU0ZmJjSlFNIiwiYWxnIjozLCJAdHlwZSI6IkNyeXB0b1NlY3JldEtleSJ9")
+
+                    expect(derived.toBase64()).to.not.equal(derivedComparison.toBase64())
+                })
+
+                it("should derive different keys of different input (salt)", async function() {
+                    const derived = await CryptoDerivation.deriveKeyFromPassword(master, salt, CryptoEncryptionAlgorithm.XCHACHA20_POLY1305, CryptoDerivationAlgorithm.ARGON2ID, 10)
+                    expect(derived).to.be.instanceOf(CryptoSecretKey);
+                    expect(derived.toBase64()).to.equal("eyJrZXkiOiI5ck1uY2NOODlsRVpXNVJuQWdpWWk4Tm9xY21vOWIyMmFYQmpuMTlRV0ZRIiwiYWxnIjozLCJAdHlwZSI6IkNyeXB0b1NlY3JldEtleSJ9")
+
+                    const salt2 = CoreBuffer.from((await CryptoHash.hash(CoreBuffer.fromString("test2", Encoding.Utf8), CryptoHashAlgorithm.SHA256)).buffer.subarray(0,16));
+                    const derivedComparison = await CryptoDerivation.deriveKeyFromPassword(master, salt2, CryptoEncryptionAlgorithm.XCHACHA20_POLY1305, CryptoDerivationAlgorithm.ARGON2ID, 10)
+                    expect(derivedComparison).to.be.instanceOf(CryptoSecretKey);
+                    expect(derivedComparison.toBase64()).to.equal("eyJrZXkiOiJiaGxybXZPdUxZWUZMVXpRaVh5VHdrTjl4Z1RNZlVhOFN3SDdQcU1nWnFVIiwiYWxnIjozLCJAdHlwZSI6IkNyeXB0b1NlY3JldEtleSJ9")
+
+                    expect(derived.toBase64()).to.not.equal(derivedComparison.toBase64())
+                })
+
+                it("should derive different keys of different input (iterations)", async function() {
+                    const derived = await CryptoDerivation.deriveKeyFromPassword(master, salt, CryptoEncryptionAlgorithm.XCHACHA20_POLY1305, CryptoDerivationAlgorithm.ARGON2ID, 1)
+                    expect(derived).to.be.instanceOf(CryptoSecretKey);
+                    expect(derived.toBase64()).to.equal("eyJrZXkiOiI4OE1XZWItdXlQRkhkOWJRZENJY05iSHlpbXFhUUo2TXdfekxxemdBTjQwIiwiYWxnIjozLCJAdHlwZSI6IkNyeXB0b1NlY3JldEtleSJ9")
+
+                    const derivedComparison = await CryptoDerivation.deriveKeyFromPassword(master, salt, CryptoEncryptionAlgorithm.XCHACHA20_POLY1305, CryptoDerivationAlgorithm.ARGON2ID, 2)
+                    expect(derivedComparison).to.be.instanceOf(CryptoSecretKey);
+                    expect(derivedComparison.toBase64()).to.equal("eyJrZXkiOiJiTDVYWkM3YTBEQWhUS1g4T2JsZVR0Mk0yNXJhOXctOGdnYmJ0eHRrdndRIiwiYWxnIjozLCJAdHlwZSI6IkNyeXB0b1NlY3JldEtleSJ9")
+
+                    expect(derived.toBase64()).to.not.equal(derivedComparison.toBase64())
+                })
+
+                it("should derive different keys of different input (memlimit)", async function() {
+                    const derived = await CryptoDerivation.deriveKeyFromPassword(master, salt, CryptoEncryptionAlgorithm.XCHACHA20_POLY1305, CryptoDerivationAlgorithm.ARGON2ID, 1, 8192)
+                    expect(derived).to.be.instanceOf(CryptoSecretKey);
+                    expect(derived.toBase64()).to.equal("eyJrZXkiOiI4OE1XZWItdXlQRkhkOWJRZENJY05iSHlpbXFhUUo2TXdfekxxemdBTjQwIiwiYWxnIjozLCJAdHlwZSI6IkNyeXB0b1NlY3JldEtleSJ9")
+
+
+                    //Careful here - increasing memlimit too little does not change the derived key
+                    const derivedComparison = await CryptoDerivation.deriveKeyFromPassword(master, salt, CryptoEncryptionAlgorithm.XCHACHA20_POLY1305, CryptoDerivationAlgorithm.ARGON2ID, 1, 8192*2)
+                    expect(derivedComparison).to.be.instanceOf(CryptoSecretKey);
+                    expect(derivedComparison.toBase64()).to.equal("eyJrZXkiOiJVV05obGtua0U0Tmx5bWUtdUh4c2x4M09MbzBjTjUtSGdFV0dtdHJpQmVrIiwiYWxnIjozLCJAdHlwZSI6IkNyeXB0b1NlY3JldEtleSJ9")
+
+                    expect(derived.toBase64()).to.not.equal(derivedComparison.toBase64())
+                })
+            });
+            
+        });
+    }
+}
