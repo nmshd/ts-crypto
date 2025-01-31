a1e01c5539b4b978b576d40f2a70fdbd2ada273b
Adam McKellar <dev@mckellar.eu>
2025-01-31 20:29:03 +0100

    feat: extended signature private and public key with crypto layer functionality

--- src/signature/CryptoSignaturePrivateKey.ts
+++ src/signature/CryptoSignaturePrivateKey.ts
@@ -1,4 +1,8 @@
 import { ISerializable, ISerialized, serialize, type, validate } from "@js-soft/ts-serval";
+import { Provider } from "crypto-layer-ts-types";
+import { CryptoError } from "src/CryptoError";
+import { CryptoErrorCode } from "src/CryptoErrorCode";
+import { CryptoLayerKeyPair } from "src/CryptoLayerKeyPair";
 import { CoreBuffer, IClearable, ICoreBuffer } from "../CoreBuffer";
 import { CryptoPrivateKey } from "../CryptoPrivateKey";
 import { CryptoSignatureAlgorithm } from "./CryptoSignatureAlgorithm";
@@ -7,13 +11,15 @@ import { CryptoSignatures } from "./CryptoSignatures";
 import { CryptoSignatureValidation } from "./CryptoSignatureValidation";
 
 export interface ICryptoSignaturePrivateKeySerialized extends ISerialized {
-    alg: number;
-    prv: string;
+    alg: number; // algorithm
+    prv?: string; // privateKey
     id?: string;
+    kid?: string; // keyId
+    pnm?: string; // providerName
 }
 export interface ICryptoSignaturePrivateKey extends ISerializable {
     algorithm: CryptoSignatureAlgorithm;
-    privateKey: ICoreBuffer;
+    privateKey: ICoreBuffer | CryptoLayerKeyPair;
     id?: string;
 }
 
@@ -26,24 +32,65 @@ export class CryptoSignaturePrivateKey extends CryptoPrivateKey implements ICryp
     public id?: string;
 
     public override toJSON(verbose = true): ICryptoSignaturePrivateKeySerialized {
+        if (this.privateKey instanceof CoreBuffer) {
+            return {
+                prv: this.privateKey.toBase64URL(),
+                alg: this.algorithm,
+                id: this.id,
+                "@type": verbose ? "CryptoSignaturePrivateKey" : undefined
+            };
+        }
+
+        if (!this.privateKey.keyPairHandle || !this.privateKey.provider) {
+            throw new CryptoError(
+                CryptoErrorCode.CalUninitializedKey,
+                "The key pair does not hold a key pair handle. It needs to be loaded from a provider via the init method."
+            );
+        }
         return {
-            prv: this.privateKey.toBase64URL(),
             alg: this.algorithm,
-            id: this.id,
-            "@type": verbose ? "CryptoSignaturePrivateKey" : undefined
+            kid: this.privateKey.keyPairHandle.id(),
+            pnm: this.privateKey.provider.providerName()
         };
     }
 
     public clear(): void {
-        this.privateKey.clear();
+        if (this.privateKey instanceof CoreBuffer) {
+            this.privateKey.clear();
+        }
     }
 
     public override toBase64(verbose = true): string {
         return CoreBuffer.utf8_base64(this.serialize(verbose));
     }
 
-    public async toPublicKey(): Promise<CryptoSignaturePublicKey> {
-        return await CryptoSignatures.privateKeyToPublicKey(this);
+    /**
+     * Returns the public key of this private key.
+     *
+     * @param provider If defined, will create the public key with this provider instead of the provider used by the private key.
+     * @returns CryptoSignaturePublicKey
+     *
+     * @throws `CryptoErrorCode.CalUninitializedKey` - If `privateKey` is an uninitialized crypto layer key pair handle.
+     */
+    public async toPublicKey(provider?: Provider): Promise<CryptoSignaturePublicKey> {
+        if (this.privateKey instanceof CoreBuffer) {
+            return await CryptoSignatures.privateKeyToPublicKey(this);
+        }
+
+        provider = provider ?? this.privateKey.provider;
+        if (!this.privateKey.keyPairHandle || !provider) {
+            throw new CryptoError(
+                CryptoErrorCode.CalUninitializedKey,
+                "The key pair does not hold a key pair handle. It needs to be loaded from a provider via the init method."
+            );
+        }
+
+        let rawPublicKey = new CoreBuffer(this.privateKey.keyPairHandle.getPublicKey());
+        let spec = this.privateKey.keyPairHandle.spec();
+        let newKeyPair = provider.importPublicKey(spec, rawPublicKey.buffer);
+        rawPublicKey.clear();
+        let cryptoLayerKeyPair = new CryptoLayerKeyPair(provider, newKeyPair);
+        return CryptoSignaturePublicKey.fromAny({ algorithm: this.algorithm, publicKey: rawPublicKey });
     }
 
     public static override from(
@@ -59,10 +106,20 @@ export class CryptoSignaturePrivateKey extends CryptoPrivateKey implements ICryp
                 privateKey: value.prv,
                 id: value.id
             };
+        } else if (value.kid && value.pnm) {
+            let keyPairHandle = CryptoLayerKeyPair.fromAny({ id: value.kid, providerName: value.pnm });
+            value = {
+                algorithm: value.alg,
+                privateKey: keyPairHandle,
+                id: value.id
+            };
         }
 
         CryptoSignatureValidation.checkSignatureAlgorithm(value.algorithm);
-        CryptoSignatureValidation.checkSignaturePrivateKey(value.privateKey, "privateKey");
+
+        if (value.privateKey instanceof CoreBuffer) {
+            CryptoSignatureValidation.checkSignaturePrivateKey(value.privateKey, "privateKey");
+        }
 
         return value;
     }
--- src/signature/CryptoSignaturePublicKey.ts
+++ src/signature/CryptoSignaturePublicKey.ts
@@ -1,17 +1,22 @@
 import { ISerializable, ISerialized, type } from "@js-soft/ts-serval";
+import { CryptoError } from "src/CryptoError";
+import { CryptoErrorCode } from "src/CryptoErrorCode";
+import { CryptoLayerKeyPair } from "src/CryptoLayerKeyPair";
 import { CoreBuffer, IClearable, ICoreBuffer } from "../CoreBuffer";
 import { CryptoPublicKey } from "../CryptoPublicKey";
 import { CryptoSignatureAlgorithm } from "./CryptoSignatureAlgorithm";
 import { CryptoSignatureValidation } from "./CryptoSignatureValidation";
 
 export interface ICryptoSignaturePublicKeySerialized extends ISerialized {
-    alg: number;
-    pub: string;
+    alg: number; // algorithm
+    pub?: string; // publicKey
+    kid?: string; // keyId
+    pnm?: string; // providerName
 }
 
 export interface ICryptoSignaturePublicKey extends ISerializable {
     algorithm: CryptoSignatureAlgorithm;
-    publicKey: ICoreBuffer;
+    publicKey: ICoreBuffer | CryptoLayerKeyPair;
 }
 
 @type("CryptoSignaturePublicKey")
@@ -19,15 +24,31 @@ export class CryptoSignaturePublicKey extends CryptoPublicKey implements ICrypto
     public override algorithm: CryptoSignatureAlgorithm;
 
     public override toJSON(verbose = true): ICryptoSignaturePublicKeySerialized {
+        if (this.publicKey instanceof CoreBuffer) {
+            return {
+                alg: this.algorithm,
+                pub: this.publicKey.toBase64URL(),
+                "@type": verbose ? "CryptoSignaturePublicKey" : undefined
+            };
+        }
+
+        if (!this.publicKey.keyPairHandle || !this.publicKey.provider) {
+            throw new CryptoError(
+                CryptoErrorCode.CalUninitializedKey,
+                "The key pair does not hold a key pair handle. It needs to be loaded from a provider via the init method."
+            );
+        }
         return {
-            pub: this.publicKey.toBase64URL(),
             alg: this.algorithm,
-            "@type": verbose ? "CryptoSignaturePublicKey" : undefined
+            kid: this.publicKey.keyPairHandle.id(),
+            pnm: this.publicKey.provider.providerName()
         };
     }
 
     public clear(): void {
-        this.publicKey.clear();
+        if (this.publicKey instanceof CoreBuffer) {
+            this.publicKey.clear();
+        }
     }
 
     public override toBase64(verbose = true): string {
@@ -44,11 +65,19 @@ export class CryptoSignaturePublicKey extends CryptoPublicKey implements ICrypto
                 algorithm: value.alg,
                 publicKey: value.pub
             };
+        } else if (value.kid && value.pnm) {
+            let keyPairHandle = CryptoLayerKeyPair.fromAny({ id: value.kid, providerName: value.pnm });
+            value = {
+                algorithm: value.alg,
+                publicKey: keyPairHandle
+            };
         }
 
         CryptoSignatureValidation.checkSignatureAlgorithm(value.algorithm);
 
-        CryptoSignatureValidation.checkSignaturePublicKey(value.publicKey, value.algorithm, "publicKey");
+        if (value.publicKey instanceof CoreBuffer) {
+            CryptoSignatureValidation.checkSignaturePublicKey(value.publicKey, value.algorithm, "publicKey");
+        }
 
         return value;
     }
