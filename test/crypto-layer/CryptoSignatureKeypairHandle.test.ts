ca88abc6f93e402148814b4285034d042d8f84b4
Adam McKellar <dev@mckellar.eu>
2025-03-04 15:11:22 +0100

    test: parameterized test for signature key pair creation

--- test/crypto-layer/CryptoSignatureKeypairHandle.test.ts
+++ test/crypto-layer/CryptoSignatureKeypairHandle.test.ts
@@ -1,53 +1,49 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 import { CryptoSignatures } from "@nmshd/crypto";
 import { KeyPairSpec } from "@nmshd/rs-crypto-types";
 import { expect } from "chai";
 import { CryptoSignatureKeypairHandle } from "src/crypto-layer/signature/CryptoSignatureKeypair";
 import { CryptoSignaturePrivateKeyHandle } from "src/crypto-layer/signature/CryptoSignaturePrivateKeyHandle";
 import { CryptoSignaturePublicKeyHandle } from "src/crypto-layer/signature/CryptoSignaturePublicKeyHandle";
+import { parameterizedKeyPairSpec } from "./CryptoLayerTestUtil";
+
+async function expectCryptoSignatureKeypairHandle(
+    cryptoKeyPairHandle: CryptoSignatureKeypairHandle,
+    expectedProvider: string,
+    spec: KeyPairSpec
+): Promise<void> {
+    expect(cryptoKeyPairHandle).to.be.instanceOf(CryptoSignatureKeypairHandle);
+    expect(cryptoKeyPairHandle.privateKey).to.be.instanceOf(CryptoSignaturePrivateKeyHandle);
+    expect(cryptoKeyPairHandle.publicKey).to.be.instanceOf(CryptoSignaturePublicKeyHandle);
+    expect(cryptoKeyPairHandle.privateKey.keyPairHandle).to.be.ok.and.deep.equal(
+        cryptoKeyPairHandle.publicKey.keyPairHandle
+    );
+    expect(cryptoKeyPairHandle.privateKey.id)
+        .to.be.a("string")
+        .and.to.equal(await cryptoKeyPairHandle.privateKey.keyPairHandle.id());
+    expect(cryptoKeyPairHandle.publicKey.id)
+        .to.be.a("string")
+        .and.to.equal(await cryptoKeyPairHandle.publicKey.keyPairHandle.id())
+        .and.to.be.string(cryptoKeyPairHandle.privateKey.id);
+
+    expect(cryptoKeyPairHandle.privateKey.provider).to.be.ok.and.deep.equal(cryptoKeyPairHandle.publicKey.provider);
+    expect(cryptoKeyPairHandle.privateKey.providerName)
+        .to.be.a("string")
+        .and.to.be.string(cryptoKeyPairHandle.publicKey.providerName)
+        .and.to.be.string(expectedProvider);
+    expect(await cryptoKeyPairHandle.privateKey.keyPairHandle.spec()).to.deep.equal(spec);
+    expect(await cryptoKeyPairHandle.publicKey.keyPairHandle.spec()).to.deep.equal(spec);
+}
 
 export class CryptoSignatureKeypairHandleTest {
     public static run(): void {
         describe("CryptoSignatureKeypairHandle", function () {
             describe("generateKeyPairHandle() SoftwareProvider", function () {
-                it("generateKeyPairHandle() with P256", async function () {
-                    const spec: KeyPairSpec = {
-                        asym_spec: "P256",
-                        cipher: null,
-                        signing_hash: "Sha2_512",
-                        ephemeral: false,
-                        non_exportable: false
-                    };
+                parameterizedKeyPairSpec("generateKeyPairHandle()", async function (spec: KeyPairSpec) {
                     const cryptoKeyPairHandle = await CryptoSignatures.generateKeypairHandle(
                         { providerName: "SoftwareProvider" },
                         spec
                     );
-                    expect(cryptoKeyPairHandle).to.exist;
-                    expect(cryptoKeyPairHandle).to.be.instanceOf(CryptoSignatureKeypairHandle);
-                    expect(cryptoKeyPairHandle.privateKey).to.exist;
-                    expect(cryptoKeyPairHandle.publicKey).to.exist;
-                    expect(cryptoKeyPairHandle.privateKey).to.be.instanceOf(CryptoSignaturePrivateKeyHandle);
-                    expect(cryptoKeyPairHandle.publicKey).to.be.instanceOf(CryptoSignaturePublicKeyHandle);
-                    expect(cryptoKeyPairHandle.privateKey.keyPairHandle).to.exist;
-                    expect(cryptoKeyPairHandle.publicKey.keyPairHandle).to.exist;
-                    expect(cryptoKeyPairHandle.privateKey.keyPairHandle).to.be.equal(
-                        cryptoKeyPairHandle.publicKey.keyPairHandle
-                    );
-                    expect(cryptoKeyPairHandle.privateKey.id).to.exist;
-                    expect(cryptoKeyPairHandle.publicKey.id).to.exist;
-                    expect(cryptoKeyPairHandle.privateKey.id).to.be.string(cryptoKeyPairHandle.publicKey.id);
-                    expect(cryptoKeyPairHandle.privateKey.provider).to.exist;
-                    expect(cryptoKeyPairHandle.publicKey.provider).to.exist;
-                    expect(cryptoKeyPairHandle.privateKey.provider).to.equal(cryptoKeyPairHandle.publicKey.provider);
-                    expect(cryptoKeyPairHandle.privateKey.providerName).to.exist;
-                    expect(cryptoKeyPairHandle.publicKey.providerName).to.exist;
-                    expect(cryptoKeyPairHandle.privateKey.providerName).to.be.string(
-                        cryptoKeyPairHandle.publicKey.providerName
-                    );
-                    expect(cryptoKeyPairHandle.privateKey.providerName).to.be.string("SoftwareProvider");
-                    expect(await cryptoKeyPairHandle.privateKey.keyPairHandle.id()).to.be.string(
-                        cryptoKeyPairHandle.privateKey.id
-                    );
+                    await expectCryptoSignatureKeypairHandle(cryptoKeyPairHandle, "SoftwareProvider", spec);
                 });
             });
         });
---
+++ test/crypto-layer/CryptoSignaturePrivateKeyHandle.test.ts
@@ -0,0 +1,13 @@
+import { expect } from "chai";
+
+export class CryptoSignaturePrivateKeyHandleTest {
+    public static run(): void {
+        describe("CryptoSignaturePrivateKeyHandle", function () {
+            describe("sign() with SoftwareProvider", function () {
+                it("sign() with P256", async function () {
+                    expect(true).to.eq(true);
+                });
+            });
+        });
+    }
+}
