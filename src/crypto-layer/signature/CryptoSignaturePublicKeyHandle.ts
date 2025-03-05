b09cdf8f8bc34c59444e21ac0efe7f9fd8c9dc8c
Adam McKellar <dev@mckellar.eu>
2025-03-05 16:41:10 +0100

    revert: rawPublicKey from CryptoPublicKeyHandle

--- src/crypto-layer/signature/CryptoSignaturePublicKeyHandle.ts
+++ src/crypto-layer/signature/CryptoSignaturePublicKeyHandle.ts
@@ -7,13 +7,11 @@ export interface ICryptoSignaturePublicKeyHandleSerialized extends ISerialized {
     spc: KeyPairSpec; // Specification/Config of key pair stored.
     cid: string; // Crypto layer key pair id used for loading key from a provider.
     pnm: string; // Provider name
-    pub?: string; // Raw public key. If not undefined tries to import this key.
 }
 export interface ICryptoSignaturePublicKeyHandle extends ISerializable {
     spec: KeyPairSpec;
     id: string;
     providerName: string;
-    rawPublicKey?: CoreBuffer;
 }
 
 @type("CryptoSignaturePublicKeyHandle")
@@ -23,7 +21,6 @@ export class CryptoSignaturePublicKeyHandle extends CryptoPublicKeyHandle {
             spc: this.spec,
             cid: this.id,
             pnm: this.providerName,
-            pub: this.rawPublicKey?.toBase64URL(),
             "@type": verbose ? "CryptoSignaturePublicKeyHandle" : undefined
         };
     }
@@ -43,8 +40,7 @@ export class CryptoSignaturePublicKeyHandle extends CryptoPublicKeyHandle {
             value = {
                 spec: value.spc,
                 id: value.cid,
-                providerName: value.pnm,
-                rawPublicKey: value.pub
+                providerName: value.pnm
             };
         }
 
--- test/crypto-layer/CryptoSignaturePublicKeyHandle.test.ts
+++ test/crypto-layer/CryptoSignaturePublicKeyHandle.test.ts
@@ -1,99 +1,78 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { KeyPairSpec } from "@nmshd/rs-crypto-types";
 import { expect } from "chai";
 import { CryptoSignaturePublicKeyHandle } from "src/crypto-layer/signature/CryptoSignaturePublicKeyHandle";
 import { CryptoSignatures } from "src/signature/CryptoSignatures";
-import {
-    expectCryptoSignatureAsymmetricKeyHandle,
-    parameterizedKeyPairSpec,
-    ParametersKeyPairSpec
-} from "./CryptoLayerTestUtil";
+import { expectCryptoSignatureAsymmetricKeyHandle } from "./CryptoLayerTestUtil";
 
 export class CryptoSignaturePublicKeyHandleTest {
     public static run(): void {
         describe("CryptoSignaturePublicKeyHandle", function () {
             describe("CryptoSignaturePublicKeyHandle SoftwareProvider P256 Sha2_512", function () {
-                const matrix: ParametersKeyPairSpec = {
-                    asymSpec: ["P256"],
-                    cipher: [null],
-                    signingHash: ["Sha2_512"],
-                    ephemeral: [false],
-                    nonExportable: [false, true]
+                const spec: KeyPairSpec = {
+                    asym_spec: "P256",
+                    cipher: null,
+                    signing_hash: "Sha2_512",
+                    ephemeral: false,
+                    non_exportable: true
                 };
                 const providerIdent = { providerName: "SoftwareProvider" };
 
-                parameterizedKeyPairSpec(
-                    "toJSON() and fromJSON()",
-                    async function (spec) {
-                        const cryptoKeyPairHandle = await CryptoSignatures.generateKeypairHandle(providerIdent, spec);
-                        const publicKeyHandle = cryptoKeyPairHandle.publicKey;
-                        const id = publicKeyHandle.id;
-                        const providerName = publicKeyHandle.providerName;
+                it("toJSON() and fromJSON()", async function () {
+                    const cryptoKeyPairHandle = await CryptoSignatures.generateKeypairHandle(providerIdent, spec);
+                    const publicKeyHandle = cryptoKeyPairHandle.publicKey;
+                    const id = publicKeyHandle.id;
+                    const providerName = publicKeyHandle.providerName;
 
-                        const serializedpublicKeyHandle = publicKeyHandle.toJSON();
-                        expect(serializedpublicKeyHandle).to.be.instanceOf(Object);
-                        expect(serializedpublicKeyHandle.cid).to.equal(id);
-                        expect(serializedpublicKeyHandle.pnm).to.equal(providerName);
-                        expect(serializedpublicKeyHandle.spc).to.deep.equal(spec);
-                        expect(serializedpublicKeyHandle["@type"]).to.equal("CryptoSignaturePublicKeyHandle");
+                    const serializedpublicKeyHandle = publicKeyHandle.toJSON();
+                    expect(serializedpublicKeyHandle).to.be.instanceOf(Object);
+                    expect(serializedpublicKeyHandle.cid).to.equal(id);
+                    expect(serializedpublicKeyHandle.pnm).to.equal(providerName);
+                    expect(serializedpublicKeyHandle.spc).to.deep.equal(spec);
+                    expect(serializedpublicKeyHandle["@type"]).to.equal("CryptoSignaturePublicKeyHandle");
 
-                        const loadedpublicKeyHandle =
-                            await CryptoSignaturePublicKeyHandle.fromJSON(serializedpublicKeyHandle);
-                        await expectCryptoSignatureAsymmetricKeyHandle(loadedpublicKeyHandle, id, spec, providerName);
-                    },
-                    matrix
-                );
+                    const loadedpublicKeyHandle =
+                        await CryptoSignaturePublicKeyHandle.fromJSON(serializedpublicKeyHandle);
+                    await expectCryptoSignatureAsymmetricKeyHandle(loadedpublicKeyHandle, id, spec, providerName);
+                });
 
-                parameterizedKeyPairSpec(
-                    "toBase64() and fromBase64()",
-                    async function (spec) {
-                        const cryptoKeyPairHandle = await CryptoSignatures.generateKeypairHandle(providerIdent, spec);
-                        const publicKeyHandle = cryptoKeyPairHandle.publicKey;
-                        const id = publicKeyHandle.id;
-                        const providerName = publicKeyHandle.providerName;
+                it("toBase64() and fromBase64()", async function () {
+                    const cryptoKeyPairHandle = await CryptoSignatures.generateKeypairHandle(providerIdent, spec);
+                    const publicKeyHandle = cryptoKeyPairHandle.publicKey;
+                    const id = publicKeyHandle.id;
+                    const providerName = publicKeyHandle.providerName;
 
-                        const serializedpublicKey = publicKeyHandle.toBase64();
-                        expect(serializedpublicKey).to.be.ok;
-                        const deserializedpublicKey = CryptoSignaturePublicKeyHandle.fromBase64(serializedpublicKey);
-                        await expectCryptoSignatureAsymmetricKeyHandle(
-                            await deserializedpublicKey,
-                            id,
-                            spec,
-                            providerName
-                        );
-                    },
-                    matrix
-                );
+                    const serializedpublicKey = publicKeyHandle.toBase64();
+                    expect(serializedpublicKey).to.be.ok;
+                    const deserializedpublicKey = CryptoSignaturePublicKeyHandle.fromBase64(serializedpublicKey);
+                    await expectCryptoSignatureAsymmetricKeyHandle(await deserializedpublicKey, id, spec, providerName);
+                });
 
-                parameterizedKeyPairSpec(
-                    "from() ICryptoSignaturePublicKeyHandle",
-                    async function (spec) {
-                        const cryptoKeyPairHandle = await CryptoSignatures.generateKeypairHandle(providerIdent, spec);
-                        const publicKeyHandle = cryptoKeyPairHandle.publicKey;
-                        const id = publicKeyHandle.id;
-                        const providerName = publicKeyHandle.providerName;
+                // eslint-disable-next-line jest/expect-expect
+                it("from() ICryptoSignaturePublicKeyHandle", async function () {
+                    const cryptoKeyPairHandle = await CryptoSignatures.generateKeypairHandle(providerIdent, spec);
+                    const publicKeyHandle = cryptoKeyPairHandle.publicKey;
+                    const id = publicKeyHandle.id;
+                    const providerName = publicKeyHandle.providerName;
 
-                        const loadedpublicKeyHandle = await CryptoSignaturePublicKeyHandle.from({
-                            spec: spec,
-                            id: id,
-                            providerName: providerName
-                        });
-                        await expectCryptoSignatureAsymmetricKeyHandle(loadedpublicKeyHandle, id, spec, providerName);
-                    },
-                    matrix
-                );
+                    const loadedpublicKeyHandle = await CryptoSignaturePublicKeyHandle.from({
+                        spec: spec,
+                        id: id,
+                        providerName: providerName
+                    });
+                    await expectCryptoSignatureAsymmetricKeyHandle(loadedpublicKeyHandle, id, spec, providerName);
+                });
 
-                parameterizedKeyPairSpec(
-                    "from() CryptoSignaturePublicKeyHandle",
-                    async function (spec) {
-                        const cryptoKeyPairHandle = await CryptoSignatures.generateKeypairHandle(providerIdent, spec);
-                        const publicKeyHandle = cryptoKeyPairHandle.publicKey;
-                        const id = publicKeyHandle.id;
-                        const providerName = publicKeyHandle.providerName;
+                // eslint-disable-next-line jest/expect-expect
+                it("from() CryptoSignaturePublicKeyHandle", async function () {
+                    const cryptoKeyPairHandle = await CryptoSignatures.generateKeypairHandle(providerIdent, spec);
+                    const publicKeyHandle = cryptoKeyPairHandle.publicKey;
+                    const id = publicKeyHandle.id;
+                    const providerName = publicKeyHandle.providerName;
 
-                        const loadedpublicKeyHandle = await CryptoSignaturePublicKeyHandle.from(publicKeyHandle);
-                        await expectCryptoSignatureAsymmetricKeyHandle(loadedpublicKeyHandle, id, spec, providerName);
-                    },
-                    matrix
-                );
+                    const loadedpublicKeyHandle = await CryptoSignaturePublicKeyHandle.from(publicKeyHandle);
+                    await expectCryptoSignatureAsymmetricKeyHandle(loadedpublicKeyHandle, id, spec, providerName);
+                });
             });
         });
     }
