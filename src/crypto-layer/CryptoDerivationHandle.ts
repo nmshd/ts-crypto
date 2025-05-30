3e530f6ee91cabddcfdfb436bf0d8ca052baa985
Adam McKellar <dev@mckellar.eu>
2025-05-30 11:53:08 +0200

    feat: change derive key handle to new explicit key handle classes

--- src/crypto-layer/CryptoDerivationHandle.ts
+++ src/crypto-layer/CryptoDerivationHandle.ts
@@ -3,19 +3,21 @@ import { CoreBuffer, ICoreBuffer } from "../CoreBuffer";
 import { CryptoError } from "../CryptoError";
 import { CryptoErrorCode } from "../CryptoErrorCode";
 import { getProvider, ProviderIdentifier } from "./CryptoLayerProviders";
-import { CryptoSecretKeyHandle } from "./encryption/CryptoSecretKeyHandle";
+import { BaseKeyHandle, BaseKeyHandleConstructor } from "./encryption/BaseKeyHandle";
+import { DeviceBoundDerivedKeyHandle } from "./encryption/DeviceBoundDerivedKeyHandle";
+import { DeviceBoundKeyHandle } from "./encryption/DeviceBoundKeyHandle";
+import { PortableDerivedKeyHandle } from "./encryption/PortableDerivedKeyHandle";
+import { PortableKeyHandle } from "./encryption/PortableKeyHandle";
 
 export class CryptoDerivationHandle {
-    /**
-     * Derive an ephemeral {@link CryptoSecretKeyHandle} from a password.
-     */
-    public static async deriveKeyHandleFromPassword(
+    private static async deriveKeyHandleFromPassword<T extends BaseKeyHandle>(
+        constructor: BaseKeyHandleConstructor<T>,
         providerIdent: ProviderIdentifier,
         password: ICoreBuffer,
         salt: ICoreBuffer,
         keySpecOfResultingKey: KeySpec,
         kdfOptions: KDF
-    ): Promise<CryptoSecretKeyHandle> {
+    ): Promise<T> {
         const provider = getProvider(providerIdent);
 
         let keyHandle;
@@ -36,17 +38,55 @@ export class CryptoDerivationHandle {
             );
         }
 
-        return await CryptoSecretKeyHandle.fromProviderAndKeyHandle(provider, keyHandle);
+        return await constructor.fromProviderAndKeyHandle(provider, keyHandle);
     }
 
     /**
-     * Derive an ephemeral {@link CryptoSecretKeyHandle} from another with the same key spec and algorithm.
+     * Derive an ephemeral {@link DeviceBoundDerivedKeyHandle} from a password.
      */
-    public static async deriveKeyFromBaseKeyHandle(
-        baseKey: CryptoSecretKeyHandle,
+    public static async deriveDeviceBoundKeyHandleFromPassword(
+        providerIdent: ProviderIdentifier,
+        password: ICoreBuffer,
+        salt: ICoreBuffer,
+        keySpecOfResultingKey: KeySpec,
+        kdfOptions: KDF
+    ): Promise<DeviceBoundDerivedKeyHandle> {
+        return await CryptoDerivationHandle.deriveKeyHandleFromPassword<DeviceBoundDerivedKeyHandle>(
+            DeviceBoundDerivedKeyHandle,
+            providerIdent,
+            password,
+            salt,
+            keySpecOfResultingKey,
+            kdfOptions
+        );
+    }
+
+    /**
+     * Derive an ephemeral {@link PortableDerivedKeyHandle} from a password.
+     */
+    public static async derivePortableKeyHandleFromPassword(
+        providerIdent: ProviderIdentifier,
+        password: ICoreBuffer,
+        salt: ICoreBuffer,
+        keySpecOfResultingKey: KeySpec,
+        kdfOptions: KDF
+    ): Promise<PortableDerivedKeyHandle> {
+        return await CryptoDerivationHandle.deriveKeyHandleFromPassword<PortableDerivedKeyHandle>(
+            PortableDerivedKeyHandle,
+            providerIdent,
+            password,
+            salt,
+            keySpecOfResultingKey,
+            kdfOptions
+        );
+    }
+
+    private static async deriveKeyFromBaseKeyHandle<T extends BaseKeyHandle, R extends BaseKeyHandle>(
+        constructor: BaseKeyHandleConstructor<R>,
+        baseKey: T,
         keyId: number,
         context: string
-    ): Promise<CryptoSecretKeyHandle> {
+    ): Promise<R> {
         const bytes = CoreBuffer.fromUtf8(`id:${keyId};ctx:${context}`);
 
         let keyHandle;
@@ -62,6 +102,36 @@ export class CryptoDerivationHandle {
             );
         }
 
-        return await CryptoSecretKeyHandle.fromProviderAndKeyHandle(baseKey.provider, keyHandle);
+        return await constructor.fromProviderAndKeyHandle(baseKey.provider, keyHandle);
+    }
+
+    /**
+     * Derive an ephemeral {@link DeviceBoundDerivedKeyHandle} from a {@link DeviceBoundKeyHandle} with the same algorithms.
+     */
+    private static async deriveDeviceBoundKeyHandle(
+        baseKey: DeviceBoundKeyHandle,
+        keyId: number,
+        context: string
+    ): Promise<DeviceBoundDerivedKeyHandle> {
+        return await CryptoDerivationHandle.deriveKeyFromBaseKeyHandle<
+            DeviceBoundKeyHandle,
+            DeviceBoundDerivedKeyHandle
+        >(DeviceBoundDerivedKeyHandle, baseKey, keyId, context);
+    }
+
+    /**
+     * Derive an ephemeral {@link PortableDerivedKeyHandle} from a {@link PortableKeyHandle} with the same algorithms.
+     */
+    private static async derivePortableKeyHandle(
+        baseKey: PortableKeyHandle,
+        keyId: number,
+        context: string
+    ): Promise<PortableDerivedKeyHandle> {
+        return await CryptoDerivationHandle.deriveKeyFromBaseKeyHandle<PortableKeyHandle, PortableDerivedKeyHandle>(
+            PortableDerivedKeyHandle,
+            baseKey,
+            keyId,
+            context
+        );
     }
 }
