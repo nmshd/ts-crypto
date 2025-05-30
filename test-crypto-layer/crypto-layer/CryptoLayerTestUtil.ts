737499ac047f269560ab32da7751b9fc1a87e523
Adam McKellar <dev@mckellar.eu>
2025-05-30 12:50:58 +0200

    test: added new non_exportable functionality of key handle to parameterized test util

--- test-crypto-layer/crypto-layer/CryptoLayerTestUtil.ts
+++ test-crypto-layer/crypto-layer/CryptoLayerTestUtil.ts
@@ -5,13 +5,15 @@ export interface ParametersKeySpec {
     cipher: Cipher[];
     signingHash: CryptoHash[];
     ephemeral: boolean[];
+    nonExportable: boolean[];
 }
 
 // Parameters should match `getProviderOrThrow(TEST_PROVIDER_IDENT).getCapabilities()`
 const DEFAULT_PARAMETRIZED_KEY_SPEC: ParametersKeySpec = {
     cipher: ["AesGcm256", "AesGcm128", "XChaCha20Poly1305"],
     signingHash: ["Sha2_256", "Sha2_512"],
-    ephemeral: [false, true]
+    ephemeral: [false, true],
+    nonExportable: [false, true]
 };
 
 export function parameterizedKeySpec(
@@ -27,14 +29,17 @@ export function parameterizedKeySpec(
     matrix.signingHash.forEach((signing_hash) => {
         matrix.cipher.forEach((cipher) => {
             matrix.ephemeral.forEach((ephemeral) => {
-                // eslint-disable-next-line jest/expect-expect
-                it(`${name} with ${signing_hash}, ${cipher}${ephemeral ? `, ephemeral` : ""}`, async function () {
-                    const spec: KeySpec = {
-                        cipher: cipher,
-                        signing_hash: signing_hash,
-                        ephemeral: ephemeral
-                    };
-                    await testFunction(spec);
+                matrix.nonExportable.forEach((nonExportable) => {
+                    // eslint-disable-next-line jest/expect-expect
+                    it(`${name} with ${signing_hash}, ${cipher}${ephemeral ? `, ephemeral` : ""}${nonExportable ? `, non_exportable` : ""}`, async function () {
+                        const spec: KeySpec = {
+                            cipher: cipher,
+                            signing_hash: signing_hash,
+                            ephemeral: ephemeral,
+                            non_exportable: nonExportable
+                        };
+                        await testFunction(spec);
+                    });
                 });
             });
         });
